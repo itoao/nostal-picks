3564a9cd0cbf60bc43836e58da7c474c697af3e8
aoito <ao.ito@tabian.co>
2023-03-27 04:08:42 +0900

    ➕ vuetify導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+const srcDir = 'src/'
 
+export default defineNuxtConfig({
+  ssr: false,
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.css'
+  ],
+  excludeAliases: {
+    alias: [
+      '@',
+      '@@'
+    ]
+  },
+  build: {
+    transpile: ['vuetify']
+  },
+  srcDir,
+  vite: {
+    define: {
+      'window.global': {}
+    },
+    // https://stackoverflow.com/questions/70938763/build-problem-with-react-vitejs-and-was-amplify
+    resolve: {
+      alias: {
+        './runtimeConfig': './runtimeConfig.browser'
+      }
+    }
+  },
+  runtimeConfig: {
+    public: {
+      API_KEY: process.env.API_KEY
+    }
+  }
 })
